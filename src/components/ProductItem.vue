d333099674369f1c127490dd5b0a0905258b4865
Виктор <witja245@yandex.ru>
2021-11-15 15:33:49 +0300

    main
    Добаdление компонента ProductItem

---
+++ src/components/ProductItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="col-lg-4 col-md-4 col-sm-6" v-for="(product, index) in products" :key="index">
+    <div class="f_p_item">
+      <div class="f_p_img">
+        <img class="img-fluid" :src="product.image" :alt="product.title">
+        <div class="p_icon">
+          <a href="#"><i class="lnr lnr-heart"></i></a>
+          <a href="#"><i class="lnr lnr-cart"></i></a>
+        </div>
+      </div>
+      <a href="#"><h4>{{ product.title }}</h4></a>
+      <h5>{{ product.price }} &#8381;</h5>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ['products'],
+};
+</script>
--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,22 +1,14 @@
 <template>
   <div class="latest_product_inner row">
-    <div class="col-lg-4 col-md-4 col-sm-6" v-for="(product, index) in products" :key="index">
-      <div class="f_p_item">
-        <div class="f_p_img">
-          <img class="img-fluid" :src="product.image" :alt="product.title">
-          <div class="p_icon">
-            <a href="#"><i class="lnr lnr-heart"></i></a>
-            <a href="#"><i class="lnr lnr-cart"></i></a>
-          </div>
-        </div>
-        <a href="#"><h4>{{ product.title }}</h4></a>
-        <h5>{{ product.price }} &#8381;</h5>
-      </div>
-    </div>
+    <ProductItem :products="products"/>
   </div>
 </template>
 <script>
+import ProductItem from './ProductItem.vue';
+
 export default {
   props: ['products'],
+  components: { ProductItem },
+
 };
 </script>
